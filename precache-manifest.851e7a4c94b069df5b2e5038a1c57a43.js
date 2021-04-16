14013620524630350a27e9a89a6c668161d66518
Anabela Tavares <anabelaatavares@gmail.com>
2021-04-16 13:58:35 +0100

    Updates

---
+++ precache-manifest.851e7a4c94b069df5b2e5038a1c57a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48097417aaa8a8177296",
+    "url": "/static/css/main.9a24ae24.chunk.css"
+  },
+  {
+    "revision": "48097417aaa8a8177296",
+    "url": "/static/js/main.58391e23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "267c371138914c3a1451",
+    "url": "/static/js/2.35b22f56.chunk.js"
+  },
+  {
+    "revision": "9b6ec10d129339a5c7be34a47af9026e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-cv/precache-manifest.98ef502df713b3dbb2ce12a8e3af4dc3.js"
+  "/precache-manifest.851e7a4c94b069df5b2e5038a1c57a43.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-cv/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
