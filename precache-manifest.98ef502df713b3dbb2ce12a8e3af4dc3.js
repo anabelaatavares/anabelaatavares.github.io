6f6f331ec8cdce0e7109e27252cc508348ab2e6e
Anabela Tavares <anabelaatavares@gmail.com>
2021-04-16 13:49:32 +0100

    Updates

---
+++ precache-manifest.98ef502df713b3dbb2ce12a8e3af4dc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f4d66bad73b3d3dcf69",
+    "url": "/my-cv/static/css/main.9a24ae24.chunk.css"
+  },
+  {
+    "revision": "4f4d66bad73b3d3dcf69",
+    "url": "/my-cv/static/js/main.f00553f9.chunk.js"
+  },
+  {
+    "revision": "609b4c77da02541e8b10",
+    "url": "/my-cv/static/js/runtime~main.35ffd8d8.js"
+  },
+  {
+    "revision": "267c371138914c3a1451",
+    "url": "/my-cv/static/js/2.35b22f56.chunk.js"
+  },
+  {
+    "revision": "fafe3ac5efe5d5f0587035984457094d",
+    "url": "/my-cv/index.html"
+  }
+];
